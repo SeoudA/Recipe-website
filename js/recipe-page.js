7e2ad653266eb32d8f18d04e944487c4809ffbe0
AshlandOrk <110945631+AshlandOrk@users.noreply.github.com>
2022-10-10 10:26:55 -0400

    Clear out bad code, add comment to-do list

--- js/recipe-page.js
+++ js/recipe-page.js
@@ -1,3 +1,8 @@
-function populateRecipe(referencefile) {
-  let instructionDiv = document.getElementsByClassName("instructionDiv")[0];
-}
+// To do
+// 1. Get the JSON object to be referenced within this script
+// 2. Convert that JSON object to text and then parse that text into a javascript object
+// 3. Begin using the new javascript objects to create a new page using DOM:
+//   a. Create the article body
+//   b. Create the recipe sidebar
+//   c. Create the main article instructions div
+//   d. Insert the photo
